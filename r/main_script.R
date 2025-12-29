6a71bf68351e2fa9d266306d6bcdb3c1692455a5
Maximilian Belschner <maxibelschner@gmx.at>
2025-12-29 16:25:33 +0100

    edited the gitignore to exclude .Rproj.user directory

--- r/main_script.R
+++ r/main_script.R
@@ -10,13 +10,29 @@ pacman::p_load(data.table,
                TTR,
                ggplot2)
 
+# ===== Paths ==================================================================
+
+price_data_path = file.path("price_data")
+labelled_output_path = file.path("labelled_data")
+
+#Konfiguration
+EPIC = "GOLD"
+INTERVAL = "MINUTE_15"
+prices_filename = file.path(price_data_path, paste0(EPIC, "_", INTERVAL, ".csv"))
+
+#Lade Daten
+dt = fread(prices_filename)
+
+# Wir sehen uns zuerst einmal nur 2023-2025 an um nicht zu große Dateien zu laden
+dt_small = dt[time >= "2023-01-01" & time <= "2025-12-31"]
+
 # 1. Lade beide Scripts
-source("triple_barrier_labeling.R")
-source("alternative_labeling_methods.R")
+source("r/01_triple_barrier_labeling.R")
+source("r/01_alternative_labeling_methods.R")
 
 # 2. Starte mit Triple Barrier
 labeled_gold <- create_triple_barrier_labels(
-  prices = gold_15min,
+  prices = dt_small,
   atr_period = 14,
   atr_mult_barrier = 1.5,    # Symmetrisch!
   max_horizon_bars = 24,     # 6h
@@ -28,5 +44,4 @@ labeled_gold <- create_triple_barrier_labels(
 analyze_label_quality(labeled_gold)
 plot_label_distribution(labeled_gold)
 
-# 4. Optional: Parameter optimieren
-best_params <- optimize_labeling_parameters(gold_15min)
+# 4.
